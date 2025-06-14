943170d8dcbce6b596c3c1fd4ac5208c0fffd8fc
Caleb Gardner <calebq23@gmail.com>
2025-06-14 06:57:11 -0500

    Small...

--- src/reader/metadata.zig
+++ src/reader/metadata.zig
@@ -1,1 +1,7 @@
-pub const MetadataReader = struct{}
+const PReader = @import("../util/preader.zig").PReader;
+
+pub fn MetadataReader(comptime T: type) type {
+    return struct {
+        rdr: PReader(T)
+    };
+}
