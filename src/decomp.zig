b822e8ba43b4c0fb9bae0206ba1e4aa3e7e67eec
Caleb J. Gardner <calebq23@gmail.com>
2025-08-17 13:54:28 -0500

    More work on DecompMgr

--- src/decomp.zig
+++ src/decomp.zig
@@ -1,6 +1,6 @@
 const std = @import("std");
 
-const DecmpMgr = @This();
+const DecompMgr = @This();
 
 pub const Compression = enum(u16) {
     gzip = 1,
@@ -10,15 +10,26 @@ pub const Compression = enum(u16) {
     lz4,
     zstd,
 };
-pub const DecmpThread = struct {
+const DecompThread = struct {
     thr: std.Thread = undefined,
     fut_atomic: std.atomic.Value(u32) = .init(0),
     fut: std.Thread.Futex = .{},
 
-    pub fn start(self: *DecmpThread, mgr: *DecmpMgr) !void {
-        self.thr = try std.Thread.spawn(.{}, decompThread, .{mgr});
+    fn start(self: *DecompThread, mgr: *DecompMgr) !void {
+        self.thr = try std.Thread.spawn(.{}, thread, .{ self, mgr });
+    }
+
+    fn thread(self: *DecompThread, mgr: *DecompMgr) void {
+        while (!mgr.closed) {
+            self.fut.wait(&self.fut_atomic, 0);
+            if (mgr.closed) return;
+            if (self.fut_atomic.load(.acq_rel) == 0) continue; // Check for random wakeup.
+            defer self.fut_atomic.store(0, .acq_rel);
+        }
     }
 };
+
+const ThreadNode = std.SinglyLinkedList(DecompThread).Node;
 
 alloc: std.mem.Allocator,
 comp: Compression,
@@ -26,26 +37,52 @@ comp: Compression,
 closed: bool = false,
 to_start: usize,
 
-threads: []std.Thread,
+thrs: []DecompThread,
 
-pub fn init(alloc: std.mem.Allocator, thread_count: u32, comp: Compression) !DecmpMgr {
+thrs_queue: std.SinglyLinkedList(usize),
+thrs_mut: std.Thread.Mutex = .{},
+thrs_cond: std.Thread.Condition = .{},
+
+pub fn init(alloc: std.mem.Allocator, thread_count: usize, comp: Compression) !DecompMgr {
     return .{
         .alloc = alloc,
         .comp = comp,
         .to_start = thread_count,
-        .threads = try alloc.alloc(std.Thread, thread_count),
+        .thrs = try alloc.alloc(DecompThread, thread_count),
     };
 }
-pub fn deinit(self: DecmpMgr) void {
+pub fn deinit(self: *DecompMgr) void {
     self.closed = true;
-    for (self.threads) |t| {
-        t.join();
+    for (self.threads) |*t| {
+        t.fut_atomic.store(1, .acq_rel);
+        t.thr.join();
     }
     self.alloc.free(self.threads);
 }
 
-pub fn decomp(self: *DecmpMgr, alloc: std.mem.Allocator, dat: []u8) ![]u8 {}
+pub fn decomp(self: *DecompMgr, alloc: std.mem.Allocator, dat: []u8) ![]u8 {
+    var thr: usize = undefined;
+    self.thrs_mut.lock();
+    while (true) {
+        errdefer self.thrs_mut.unlock();
+        const node = self.thrs_queue.popFirst();
+        if (node != null) {
+            thr = node.?.data;
+            break;
+        }
+        if (self.to_start > 0) {
+            self.thrs[self.thrs.len - self.to_start] = .{};
+            try self.thrs[self.thrs.len - self.to_start].start(self);
+            thr = self.thrs.len - self.to_start;
+            self.to_start -= 1;
+            break;
+        }
+        self.thrs_cond.wait(self.thrs_mut);
+    }
+    self.thrs_mut.unlock();
+    //TODO: Actually send data to the threads
+}
 
-fn decompThread(self: *DecmpMgr) void {
+fn decompThread(self: *DecompMgr) void {
     while (!self.closed) {}
 }
